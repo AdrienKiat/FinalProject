f7ed7b2632b314fe71b0c0488169c872bc39f1e7
Adrien KIAT <adrien.kiat15@gmail.com>
2022-05-19 22:27:42 -0400

    Stage title and Game title

--- src/main/java/com/example/javafxproject/HelloApplication.java
+++ src/main/java/com/example/javafxproject/HelloApplication.java
@@ -1,19 +1,39 @@
 package com.example.javafxproject;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.geometry.Pos;
 
-import java.io.IOException;
+
 
 public class HelloApplication extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
 
-    }
 
     public static void main(String[] args) {
+        //Launch the application
         launch();
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        //Set stage title
+        primaryStage.setTitle("My reaction game");
+
+        Label messageLabel = new Label("Sharpshooter");
+
+        VBox vbox = new VBox(messageLabel);
+
+        vbox.setAlignment(Pos.CENTER);
+
+        Scene scene = new Scene(vbox , 500, 300);
+
+        primaryStage.setScene(scene);
+
+        //Show window
+        primaryStage.show();
     }
 }
